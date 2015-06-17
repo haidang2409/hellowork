5110ae9eccb951cc6dea3ef97ce469ba9ef69552
haidang2409 <haidangdhct24@gmail.com>
2015-06-17 21:25:10 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var http = require('http');
 
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World\n');
-}).listen(8080);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+});
 
-console.log('Server started');
+
