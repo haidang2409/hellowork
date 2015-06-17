2edc69cbde8f691537246cb40f320cbf90aa9062
haidang2409 <haidangdhct24@gmail.com>
2015-06-17 22:02:44 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var express = require('express')
 var app = express();
 
 app.get('/', function(req, res){
-	res.send('Hello Express!');
+	res.send('Hello Express! I am Nguyen Hai Dang');
 });
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
